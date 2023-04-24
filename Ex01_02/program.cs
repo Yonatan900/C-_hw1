c9a7c3923684fd523a1feda3816b508c3391b843
Roy Yotam <roy.yotam@post.runi.ac.il>
2023-04-24 20:16:36 +0300

    v

---
+++ Ex01_02/program.cs
@@ -0,0 +1,12 @@
+﻿susing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_02
+{
+    internal class program
+    {
+    }
+}
