6a828e534169fa5882c971cf7e82ac5ce5f2b716
RoyYotam <95774049+RoyYotam@users.noreply.github.com>
2023-04-24 20:18:54 +0300

    Update program.cs

--- Ex01_02/program.cs
+++ Ex01_02/program.cs
@@ -1,12 +1,49 @@
-﻿susing System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
 namespace Ex01_02
 {
-    internal class program
-    {
+	public class Program
+	{
+        private const int k_StartNumOfAsteriskInLine = 1;
+        private const int k_MaxNumOfAsteriskInLine = 9;
+
+        static void Main()
+		{
+
+			PrintStartersDiamond();
+		}
+
+		static void PrintStartersDiamond()
+		{
+            PrintDiamondRecursive(k_StartNumOfAsteriskInLine, k_MaxNumOfAsteriskInLine);
+
+        }
+
+		public static void PrintDiamondRecursive(int i_NumOfAsterisk, int i_MaxNumOfAsterisk)
+		{
+            PrintDiamondLine(i_NumOfAsterisk, i_MaxNumOfAsterisk);
+
+            if (i_NumOfAsterisk < i_MaxNumOfAsterisk)
+			{
+                PrintDiamondRecursive(i_NumOfAsterisk + 2, i_MaxNumOfAsterisk);
+                PrintDiamondLine(i_NumOfAsterisk, i_MaxNumOfAsterisk);
+            }
+		}
+
+        static void PrintDiamondLine(int i_NumOfAsterisk, int i_MaxNumOfAsterisk)
+        {
+            for (int i = 0; i < i_MaxNumOfAsterisk - i_NumOfAsterisk; ++i)
+            {
+                Console.Write(' ');
+            }
+
+            for (int i = 0; i < i_NumOfAsterisk; ++i)
+            {
+                Console.Write("* ");
+            }
+            Console.WriteLine("\n");
+        }
+
     }
 }
+
