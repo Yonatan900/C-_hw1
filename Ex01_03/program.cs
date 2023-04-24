8a25ac7ababdfd416d402b73728edd21cb7d3481
RoyYotam <95774049+RoyYotam@users.noreply.github.com>
2023-04-24 20:19:52 +0300

    Update program.cs

--- Ex01_03/program.cs
+++ Ex01_03/program.cs
@@ -1,12 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using Ex01_02;
 
 namespace Ex01_03
 {
-    internal class program
+    public class Program
     {
+        private const int k_StartNumOfAsteriskInLine = 1;
+        private static int s_MaxNumOfAsteriskInLine = 9;
+
+        static void Main()
+        {
+
+            PrintStartersDiamond();
+        }
+
+        static void PrintStartersDiamond()
+        {
+            s_MaxNumOfAsteriskInLine = GetMaxAsterisk();
+            Ex01_02.Program.PrintDiamondRecursive(k_StartNumOfAsteriskInLine, s_MaxNumOfAsteriskInLine);
+
+        }
+
+        static int GetMaxAsterisk()
+        {
+            int o_NumberOfLines;
+            string numberOfLinesString;
+
+            Console.WriteLine("Enter number of lines");
+            numberOfLinesString = Console.ReadLine();
+            while(!int.TryParse(numberOfLinesString, out o_NumberOfLines))
+            {
+                Console.WriteLine("Only number please.");
+            }
+
+            return o_NumberOfLines;
+        }
+
     }
 }
+
