3c78545dfdf5cf3f6ab6953cb47cce2808be9a1a
Yonatan <yonatan5556201@gmail.com>
2023-04-24 22:22:06 +0300

    did ex-5

--- Ex01_05/program.cs
+++ Ex01_05/program.cs
@@ -1,88 +1,138 @@
 using System;
-using Microsoft.AspNetCore.Components.Forms;
 
-namespace Ex01_04
+namespace Ex01_05
 {
-	public class Program
-	{
-		static void Main()
-		{
-			doSomething();
+    public class Program
+    {
+        private static String s_sixDigit;
 
-        }
-
-		static void doSomething()
-		{
-			string validInput;
-
-
-            validInput = receiveInput();
-
-			PrintFunctionOutputData(validInput);
-
-        }
-
-		static string receiveInput()
-		{
-            string inputFromUser;
-
-            Console.WriteLine("Please enter 6 chars or 6 digits.");
-            inputFromUser = Console.ReadLine();
-
-            while ((!onlyChars(inputFromUser) && !onlyDigits(inputFromUser)) || inputFromUser.Length != 6)
-			{
-                Console.WriteLine("Your input should be 6 digits only or chars only!");
-                inputFromUser = Console.ReadLine();
-            }
-
-			return inputFromUser;
-		}
-
-		static bool onlyChars(string i_InputToCheck)
-		{
-			return i_InputToCheck.All(char.IsLetter);
-
-        }
-
-        static bool onlyDigits(string i_InputToCheck)
+        static void Main()
         {
-            return i_InputToCheck.All(char.IsDigit);
+            runProgram();
+        }
+
+        static void runProgram()
+        {
+            readInputNumber(out s_sixDigit);
+            printFunctionOutputData(s_sixDigit);
+        }
+        static void readInputNumber(out String o_sixDigit)
+        {
+
+            Console.WriteLine("Please enter a 6 digit number.");
+            o_sixDigit = getValidSixDigitNumberFromUser();
 
         }
 
-		static void PrintFunctionOutputData(string i_InputFromUser)
-		{
-			Console.WriteLine("The input is {0} palindrome", isPalidrome(i_InputFromUser));
+        static String getValidSixDigitNumberFromUser()
+        {
+            String validSixDigitNumber;
+            string inputNumber = Console.ReadLine();
 
-			if (onlyDigits(i_InputFromUser))
-			{
-                Console.WriteLine("The input is {0} by 3", "devided");
+            while (!checkNumberValidity(inputNumber, out validSixDigitNumber))
+            {
+                Console.WriteLine("Your input should be 6 digits number only!");
+                inputNumber = Console.ReadLine();
             }
-			else
-			{
-                Console.WriteLine("The input has {0} uppercase letters", "8");
-            }
-            
+
+            return validSixDigitNumber;
         }
 
-		static bool isPalidrome(string i_InputToCheck)
-		{
-			bool firstAndLastEquals = true;
-			bool wrapAPalindrome = true;
+        static bool checkNumberValidity(string i_InputToCheck, out String o_SixDigitNumber)
+        {
+            bool representNumber = int.TryParse(i_InputToCheck, out int _);
+            o_SixDigitNumber = i_InputToCheck;
+            bool sixDigitLength = i_InputToCheck.Length == 6;
+            bool o_IsInputValid = representNumber && sixDigitLength;
 
-			int inputFirstIndex = 0;
-			int inputLastIndex = i_InputToCheck.Length - 1;
+            return o_IsInputValid;
+        }
 
-			if (i_InputToCheck.Length > 1)
-			{
-                firstAndLastEquals = (i_InputToCheck[inputFirstIndex] == i_InputToCheck[inputLastIndex]);
-				wrapAPalindrome = isPalidrome(i_InputToCheck.Substring(inputFirstIndex + 1, inputLastIndex - 1));
-            }
 
-			return firstAndLastEquals && wrapAPalindrome;
+
+
+
+        static void printFunctionOutputData(String i_sixDigit)
+        {
+            int unitsPlaceDigit = i_sixDigit[5];
+
+
+            Console.WriteLine("There are {0} digits that are bigger than {1}", countBiggerThanUnitsDigit(i_sixDigit), unitsPlaceDigit);
+            Console.WriteLine("There lowest digit is {0}.", getLowestDigit(i_sixDigit));
+            Console.WriteLine("There are {0} digits that are divisble By three.", countDivisbleByThree(i_sixDigit));
+            Console.WriteLine("There digits average is {0}.", getDigitsAverage(i_sixDigit));
 
         }
+
+        static int countBiggerThanUnitsDigit(String i_sixDigit)
+        {
+
+
+            char unitsDigit = i_sixDigit[5];
+            int count = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (i_sixDigit[i] > unitsDigit)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+
+
+        }
+        static char getLowestDigit(String i_sixDigit)
+        {
+            char minDigit = i_sixDigit[0];
+            foreach (char digit in i_sixDigit)
+            {
+                if (minDigit > digit)
+                {
+                    minDigit = digit;
+                }
+
+            }
+
+
+
+            return minDigit;
+        }
+
+
+
+        static int countDivisbleByThree(string i_sixDigit)
+        {
+            int count = 0;
+            foreach (char digitChar in i_sixDigit)
+            {
+                int digit = (int)Char.GetNumericValue(digitChar);
+
+                if (digit % 3 == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+        static double getDigitsAverage(string i_StringToCheck)
+        {
+            double digitsSum = 0;
+            double digitsAverage;
+            foreach (char digitChar in i_StringToCheck)
+            {
+                digitsSum += Char.GetNumericValue(digitChar);
+            }
+
+            digitsAverage = digitsSum / 6;
+
+
+
+            return digitsAverage;
+        }
+
 
     }
 }
-
